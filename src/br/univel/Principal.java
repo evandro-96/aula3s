81a654edd47422b4af6bdffd608ccae8b4924d63
Evandro Pimentel de Areis <evandro-96@live.com>
2016-03-15 19:58:28 -0300

    Liberação versão 1.0.

--- src/br/univel/Principal.java
+++ src/br/univel/Principal.java
@@ -1,11 +1,16 @@
 package br.univel;
 
+/**
+ * Versão 1.0
+ * 
+ * @author evandro
+ *
+ */
+
 public class Principal {
 	
 	public static void main(String[] args){
 		System.out.println("Olá mundo");
-		System.out.println("É nois!!!");
-		
+		System.out.println("É nois!!!");	
 	}
-
 }
