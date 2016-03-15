fe523cae2eb1af364a57a3b3506a0f346bcf921a
Evandro Pimentel de Areis <evandro-96@live.com>
2016-03-15 20:10:30 -0300

    Teste

--- src/br/univel/Principal.java
+++ src/br/univel/Principal.java
@@ -10,7 +10,7 @@ package br.univel;
 public class Principal {
 	
 	public static void main(String[] args){
-		System.out.println("Olá mundo");
+		new Message().hello();
 		System.out.println("É nois!!!");	
 	}
 }
