a9dfd0a13ae1ebdc2fef3611517702b8c7e42fa6
Evandro Pimentel de Areis <evandro-96@live.com>
2016-03-15 20:19:45 -0300

    Alteração

--- src/br/univel/Principal.java
+++ src/br/univel/Principal.java
@@ -10,7 +10,7 @@ package br.univel;
 public class Principal {
 	
 	public static void main(String[] args){
-		System.out.println("Olá mundo");
-		System.out.println("É nois!!!");	
+		System.out.println("Hello World");
+		System.out.println("It's we!!!");	
 	}
 }
